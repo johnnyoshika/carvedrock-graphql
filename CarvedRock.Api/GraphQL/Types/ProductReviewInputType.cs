8e4e55ab1c3476b995a5d334c836ac79be07e57a
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-12 00:29:11 -0800

    Define 'createReview' mutation

---
+++ CarvedRock.Api/GraphQL/Types/ProductReviewInputType.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarvedRock.Api.GraphQL.Types
+{
+    public class ProductReviewInputType : InputObjectGraphType
+    {
+        public ProductReviewInputType()
+        {
+            Name = "ReviewInput";
+            Field<NonNullGraphType<StringGraphType>>("title");
+            Field<StringGraphType>("review");
+            Field<NonNullGraphType<IntGraphType>>("productId");
+        }
+    }
+}
