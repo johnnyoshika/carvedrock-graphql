8a41bf71888fd3abd0bcb4b1ab6f6ec70d836cad
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-12 18:09:15 -0800

    Add ReviewAddedMessage and its GraphQL type

---
+++ CarvedRock.Api/GraphQL/Messaging/ReviewAddedMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarvedRock.Api.GraphQL.Messaging
+{
+    public class ReviewAddedMessage
+    {
+        public int ProductId { get; set; }
+        public string Title { get; set; }
+    }
+}
