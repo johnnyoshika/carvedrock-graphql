d0fef71841625d26a95b68cb6ad344683d4ba66d
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-12 18:09:48 -0800

    Add ReviewMessageService
    This should be registered as a singleton

---
+++ CarvedRock.Api/GraphQL/Messaging/ReviewMessageService.cs
@@ -0,0 +1,29 @@
+﻿using CarvedRock.Api.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
+namespace CarvedRock.Api.GraphQL.Messaging
+{
+    public class ReviewMessageService
+    {
+        readonly ISubject<ReviewAddedMessage> _messageStream = new ReplaySubject<ReviewAddedMessage>(1);
+
+        public ReviewAddedMessage AddReviewAddedMessage(ProductReview review)
+        {
+            var message = new ReviewAddedMessage
+            {
+                ProductId = review.ProductId,
+                Title = review.Title
+            };
+
+            _messageStream.OnNext(message); // Will send the message to all subscribers
+            return message;
+        }
+
+        public IObservable<ReviewAddedMessage> GetMessages() => _messageStream.AsObservable();
+    }
+}
